9e3704af79d9ea152bb517b1151985923891de40
JiaW <gvliew@outlook.com>
2018-11-24 22:52:56 +0800

    Add files via upload

---
+++ part6:完善内核/kernel/main.c
@@ -0,0 +1,11 @@
+#include "print.h"
+void main(void) {
+   put_char('k');
+   put_char('e');
+   put_char('r');
+   put_char('n');
+   put_char('e');
+   put_char('l');
+   put_char('\n');
+   while(1);
+}
